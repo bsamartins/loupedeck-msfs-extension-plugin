fd2c145e14b6e510911d2a1d7ee7d40fa833b257
Bernardo Martins <bsamartins@gmail.com>
2023-05-03 17:11:52 +0100

    Do stuff

---
+++ src/SimConnectTest/FSConnectManagerProgram.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+using CTrue.FsConnect;
+using CTrue.FsConnect.Managers;
+
+namespace SimConnectTest
+{
+    internal class FSConnectManagerProgram
+    {
+        public enum Requests
+        {
+            PlaneInfoRequest = 0,
+            AircraftManager = 1
+        }
+
+        // Use field name and SimVar attribute to configure the data definition for the type.
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+        public struct PlaneInfoResponse
+        {
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public String Title;
+            [SimVar(Name = "L:A32NX_FLIGHT_CONTROLS_TRACKING_MODE", UnitId = FsUnit.Bool)]
+            public bool FcTrackingMode;
+            [SimVar(Name = "L:A32NX_FCU_VS_MANAGED", UnitId = FsUnit.Bool)]
+            public bool FcVsManaged;
+            [SimVar(Name = "L:A32NX_TRK_FPA_MODE_ACTIVE", UnitId = FsUnit.Bool)]
+            public bool TrackFpaModeActive;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test SimConnect");
+            var fsConnect = new FsConnect();
+            fsConnect.Connect("TestApp");
+
+            var planeInfoDefinitionId = fsConnect.RegisterDataDefinition<PlaneInfoResponse>();
+
+            var aircraftManager = new AircraftManager<PlaneInfoResponse>(fsConnect, planeInfoDefinitionId, (int) Requests.AircraftManager);
+
+            aircraftManager.Updated += HandleReceivedFsData;
+
+            // Set request method to continuously to start automatic updates using the Updated event.
+            aircraftManager.RequestMethod = RequestMethod.Continuously;
+
+            ConsoleKeyInfo cki;
+            var escape = false;
+
+            while(!escape)
+            {
+                aircraftManager.Get();
+                cki = Console.ReadKey();
+                escape = cki.Key != ConsoleKey.Escape;
+                Thread.Sleep(1000);
+            }
+
+            fsConnect.Disconnect();
+
+        }
+
+        private static void HandleReceivedFsData(Object sender, AircraftInfoUpdatedEventArgs<PlaneInfoResponse> e) {
+            var r = e.AircraftInfo;
+            Console.WriteLine($"{r.FcTrackingMode} {r.FcVsManaged} {r.TrackFpaModeActive}");           
+        }
+    }
+}
---
+++ src/SimConnectTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+using Microsoft.FlightSimulator.SimConnect;
+
+namespace SimConnectTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test SimConnect");
+            SimConnect simconnect = null;
+
+            const int WM_USER_SIMCONNECT = 0x0402;
+
+            try
+            {
+                simconnect = new SimConnect("Managed Data Request", IntPtr.Zero, WM_USER_SIMCONNECT, null, 0);
+                simconnect.OnRecvOpen += Simconnect_OnRecvOpen;
+                simconnect.OnRecvEvent += Simconnect_OnRecvEvent;
+                simconnect.OnRecvSimobjectDataBytype += Simconnect_OnRecvSimobjectDataBytype;               
+            }
+            catch (COMException ex)
+            {
+
+            }
+
+            if (simconnect != null)
+            {
+                simconnect.Dispose();
+                simconnect = null;
+            }
+
+            Thread.Sleep(-1);
+        }
+
+        private static void Simconnect_OnRecvSimobjectDataBytype(SimConnect sender, SIMCONNECT_RECV_SIMOBJECT_DATA_BYTYPE data) => throw new NotImplementedException();
+
+        private static void Simconnect_OnRecvOpen(SimConnect sender, SIMCONNECT_RECV_OPEN data) {
+            Console.WriteLine("receive open");
+        }
+
+        private static void Simconnect_OnRecvEvent(SimConnect sender, SIMCONNECT_RECV_EVENT data) {
+            Console.WriteLine("Event");
+        }
+    }
+}
